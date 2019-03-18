3ba5b5790efe0ceec18f2fca62c26b02696a5b1b
Jason.K <vonhitl@gmail.com>
2019-03-18 13:51:38 +0200

    Changes to setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='EDSA example python package',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/jkroman2/',
+    url='https://github.com/jkroman2/mypackage',
     author='Jason Kannemeyer',
     author_email='vonhitl@gmail.com'
 )
